4c97a30f1c2a4214dde0c8acd83c500c85974e8e
Oleksii Zaichenko <oleksii@platops.com>
2023-11-07 14:57:02 +0200

    added project dir

--- settings.py
+++ settings.py
@@ -2,7 +2,8 @@ import os
 import sys
 
 BASE_DIR            = os.path.dirname(os.path.abspath(__file__))
-NAMESPACES_DIR_NAME = 'namespaces'
-PODSETS_DIR_NAME    = 'podsets'
+PROJECT_DIR_NAME    = 'project'
+NAMESPACES_DIR_NAME = f'{PROJECT_DIR_NAME}/namespaces'
+PODSETS_DIR_NAME    = f'{PROJECT_DIR_NAME}/podsets'
 
 sys.path.append(BASE_DIR)
